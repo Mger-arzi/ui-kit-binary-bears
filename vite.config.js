dedba886f306dbed4179931b4261291ff59fa64c
Mger-arzi <mger54598@gmail.com>
2026-02-08 15:07:41 +0300

    1

--- vite.config.js
+++ vite.config.js
@@ -33,30 +33,30 @@ export default defineConfig({
         target: 'esnext',
         sourcemap: true,
     },
-    test: {
-      projects: [
-        {
-          extends: true,
-          plugins: [
-            storybookTest({
-              configDir: path.join(dirname, '.storybook'),
-            }),
-          ],
-          test: {
-            name: 'storybook',
-            browser: {
-              enabled: true,
-              headless: true,
-              provider: playwright({}),
-              instances: [
-                {
-                  browser: 'chromium',
-                },
-              ],
-            },
-            setupFiles: ['.storybook/vitest.setup.ts'],
-          },
-        },
-      ],
-    },
+    // test: {
+    //   projects: [
+    //     {
+    //       extends: true,
+    //       plugins: [
+    //         storybookTest({
+    //           configDir: path.join(dirname, '.storybook'),
+    //         }),
+    //       ],
+    //       test: {
+    //         name: 'storybook',
+    //         browser: {
+    //           enabled: true,
+    //           headless: true,
+    //           provider: playwright({}),
+    //           instances: [
+    //             {
+    //               browser: 'chromium',
+    //             },
+    //           ],
+    //         },
+    //         setupFiles: ['.storybook/vitest.setup.ts'],
+    //       },
+    //     },
+    //   ],
+    // },
 });
